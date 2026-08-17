f101677d86afb9ca3f7642ee221ef3f83b23263a
You55efHany <hanyyousif115@gmail.com>
2026-08-17 12:42:47 +0300

    Enhance LCA structure with max edge and distance features
    Refactor LCA structure to include maximum edge queries and distance calculations. Update build and dfs methods for binary lifting.

--- Hany/LCA.cpp
+++ Hany/LCA.cpp
@@ -1,11 +1,25 @@
-struct LCA {
+// LCA with Binary Lifting
+// Supports:
+//   - Lowest Common Ancestor
+//   - K-th ancestor
+//   - Distance between two nodes
+//
+// Preprocessing: O(N log N)
+// Query: O(log N)
+
+struct LCA
+{
     int n, LOG;
     vector<int> depth;
     vector<vector<int>> up;
 
-    void build(vector<vector<int>>& adj, int root) {
+    void build(const vector<vector<int>> &adj, int root = 1)
+    {
         n = adj.size();
-        LOG = 1 + (int)log2(n);
+        LOG = 1;
+
+        while ((1 << LOG) <= n)
+            LOG++;
 
         depth.assign(n, 0);
         up.assign(n, vector<int>(LOG));
@@ -13,135 +27,215 @@ struct LCA {
         dfs(root, root, adj);
     }
 
-    void dfs(int node, int parent, vector<vector<int>>& adj) {
-        up[node][0] = parent;
+    // Builds parent and binary lifting table
+    void dfs(int u, int parent, const vector<vector<int>> &adj)
+    {
+        up[u][0] = parent;
 
-        for (int j = 1; j < LOG; j++) {
-            up[node][j] = up[up[node][j-1]][j-1];
-        }
+        for (int j = 1; j < LOG; j++)
+            up[u][j] = up[up[u][j - 1]][j - 1];
 
-        for (auto i : adj[node]) {
-            if (i == parent) continue;
+        for (int v : adj[u])
+        {
+            if (v == parent)
+                continue;
 
-            depth[i] = depth[node] + 1;
-            dfs(i, node, adj);
+            depth[v] = depth[u] + 1;
+            dfs(v, u, adj);
         }
     }
 
-    int get_kt(int node,int k)
+    // Returns the k-th ancestor of u
+    int kthAncestor(int u, int k)
     {
-        int ans = node;
         for (int j = 0; j < LOG; j++)
         {
-            if ((k >> j) & 1)
-                ans = up[ans][j];
+            if (k & (1 << j))
+                u = up[u][j];
         }
-        return ans;
+
+        return u;
     }
 
-    int lca(int a,int b)
+    // Returns the Lowest Common Ancestor of a and b
+    int lca(int a, int b)
     {
-        if(depth[b] > depth[a])
-            swap(a,b);
+        // Make a the deeper node
+        if (depth[a] < depth[b])
+            swap(a, b);
 
-        a = get_kt(a, depth[a] - depth[b]);
+        // Bring a to the same depth as b
+        a = kthAncestor(a, depth[a] - depth[b]);
 
         if (a == b)
             return a;
 
+        // Move both nodes up while their ancestors differ
         for (int j = LOG - 1; j >= 0; j--)
         {
-            if(up[a][j] != up[b][j])
-                a = up[a][j],b = up[b][j];
+            if (up[a][j] != up[b][j])
+            {
+                a = up[a][j];
+                b = up[b][j];
+            }
         }
 
         return up[a][0];
+    }
+
+    // Returns the number of edges between a and b
+    int distance(int a, int b)
+    {
+        int lc = lca(a, b);
+
+        return depth[a] + depth[b] - 2 * depth[lc];
     }
 };
 
 
 
-// get max weight in paths
-struct LCA {
+// LCA with Binary Lifting + maximum edge on a path
+//
+// Supports:
+//   - Lowest Common Ancestor
+//   - K-th ancestor
+//   - Maximum edge from a node to its k-th ancestor
+//   - Maximum edge on the path between two nodes
+//
+// Preprocessing: O(N log N)
+// Query: O(log N)
+
+struct LCA
+{
     int n, LOG;
     vector<int> depth;
-    vector<vector<int>> up;
-    vector<vector<int>> sp;
 
-    void build(vector<vector<pair<int,int>>>& adj, int root) {
+    // up[u][j] = 2^j-th ancestor of u
+    vector<vector<int>> up;
+
+    // mx[u][j] = maximum edge on the path
+    // from u to its 2^j-th ancestor
+    vector<vector<int>> mx;
+
+    void build(const vector<vector<pair<int,int>>> &adj, int root = 1)
+    {
         n = adj.size();
-        LOG = 32 - __builtin_clz(n);
+        LOG = 1;
+
+        while ((1 << LOG) <= n)
+            LOG++;
 
         depth.assign(n, 0);
-        up.assign(n, vector<int>(LOG,root));
-        sp.assign(n, vector<int>(LOG,0));
+        up.assign(n, vector<int>(LOG, root));
+        mx.assign(n, vector<int>(LOG, 0));
 
         dfs(root, root, 0, adj);
     }
 
-    void dfs(int node, int parent,int w, vector<vector<pair<int,int>>>& adj) {
-        up[node][0] = parent;
-        sp[node][0] = w;
-        for (int j = 1; j < LOG; j++) {
-            up[node][j] = up[up[node][j-1]][j-1];
-            sp[node][j] = max(sp[node][j-1],sp[up[node][j-1]][j-1]);
+    // Builds the binary lifting and maximum-edge tables
+    void dfs(
+        int u,
+        int parent,
+        int weight,
+        const vector<vector<pair<int,int>>> &adj
+    )
+    {
+        up[u][0] = parent;
+        mx[u][0] = weight;
+
+        for (int j = 1; j < LOG; j++)
+        {
+            // 2^j-th ancestor
+            up[u][j] = up[up[u][j - 1]][j - 1];
+
+            // Maximum edge in the 2^j-length path
+            mx[u][j] = max(
+                mx[u][j - 1],
+                mx[up[u][j - 1]][j - 1]
+            );
         }
 
-        for (auto i : adj[node]) {
-            if (i.first == parent) continue;
+        for (auto [v, w] : adj[u])
+        {
+            if (v == parent)
+                continue;
 
-            depth[i.first] = depth[node] + 1;
-            dfs(i.first, node, i.second, adj);
+            depth[v] = depth[u] + 1;
+
+            dfs(v, u, w, adj);
         }
     }
 
-    int get_kt(int node,int k)
+    // Returns the k-th ancestor of u
+    int kthAncestor(int u, int k)
     {
-        int ans = node;
         for (int j = 0; j < LOG; j++)
         {
-            if ((k >> j) & 1)
-                ans = up[ans][j];
+            if (k & (1 << j))
+                u = up[u][j];
         }
-        return ans;
+
+        return u;
     }
-    
-    int get_mx(int node,int k)
+
+    // Returns the maximum edge on the path
+    // from u to its k-th ancestor
+    int maxToAncestor(int u, int k)
     {
         int ans = 0;
+
         for (int j = 0; j < LOG; j++)
         {
-            if((k>>j)&1)
+            if (k & (1 << j))
             {
-                ans = max(ans,sp[node][j]);
-                node = up[node][j];
+                ans = max(ans, mx[u][j]);
+                u = up[u][j];
             }
         }
+
         return ans;
     }
 
-    int lca(int a,int b)
+    // Returns the Lowest Common Ancestor of a and b
+    int lca(int a, int b)
     {
-        if(depth[b] > depth[a])
-            swap(a,b);
+        if (depth[a] < depth[b])
+            swap(a, b);
 
-        a = get_kt(a, depth[a] - depth[b]);
+        // Bring a to the same depth as b
+        a = kthAncestor(a, depth[a] - depth[b]);
 
         if (a == b)
             return a;
 
+        // Move both nodes upward until their parents are equal
         for (int j = LOG - 1; j >= 0; j--)
         {
-            if(up[a][j] != up[b][j])
-                a = up[a][j],b = up[b][j];
+            if (up[a][j] != up[b][j])
+            {
+                a = up[a][j];
+                b = up[b][j];
+            }
         }
 
         return up[a][0];
     }
 
-    int gt_mx_path(int a,int b)
+    // Returns the maximum edge weight on the path a -> b
+    int maxPath(int a, int b)
     {
-        int lc = lca(a,b);
-        return max(get_mx(a,depth[a]-depth[lc]),get_mx(b,depth[b]-depth[lc]));
+        int lc = lca(a, b);
+
+        int left = maxToAncestor(
+            a,
+            depth[a] - depth[lc]
+        );
+
+        int right = maxToAncestor(
+            b,
+            depth[b] - depth[lc]
+        );
+
+        return max(left, right);
     }
 };
